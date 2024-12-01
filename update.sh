7a0e998c4f9e89715fabe0fc93d1ccec1fdf12e7
Chase Reynders <chase.reynders@yale.edu>
2024-12-01 18:12:57 -0500

    longer wait to trim plots

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ mkdir train_data
 Rscript plot_generator.r $img_id
 cat new_train_img_data.json > train_img_data.json
 rm new_train_img_data.json
-sleep 0.5
+sleep 30
 /Users/chasereynders/Desktop/yale/academics/third-year/fall-semester/PLSC349/final/.venv/bin/python3 trim_plots.py $img_id
 unset img_id
