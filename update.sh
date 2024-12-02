092749be6b612e6635582574f387689570d58b2d
EC2 Default User <ec2-user@ip-172-31-12-85.us-east-2.compute.internal>
2024-12-02 03:15:49 +0000

    from the ec2

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 img_id=$(date +"%S%M%H%d%m%Y")
 rm -rf train_data
 mkdir train_data
-/Users/chasereynders/Desktop/yale/academics/third-year/fall-semester/PLSC349/final/.venv/bin/python3 data_collector.py $img_id
+/home/ec2-user/marey-live/.venv/bin/python3 data_collector.py $img_id
 Rscript plot_generator.r $img_id
 cat new_train_img_data.json > train_img_data.json
 rm new_train_img_data.json
 sleep 30
-/Users/chasereynders/Desktop/yale/academics/third-year/fall-semester/PLSC349/final/.venv/bin/python3 trim_plots.py $img_id
+/home/ec2-user/marey-live/.venv/bin/python3 trim_plots.py $img_id
 unset img_id
